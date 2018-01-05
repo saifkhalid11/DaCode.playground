7476afb5999d634fa76988470ae71c48aa380444
Muhammad Rehman <muhammadrehman@Muhammads-MBP.home>
2018-01-05 06:28:50 -0500

    Practice1

---
+++ Contents.swift
@@ -0,0 +1,95 @@
+//var wallet = "0 boook 😁"
+//var Name = "Saif"
+//var Year = 2017
+//emojis = edit --> clck emojis
+
+//write comments over and over again/**/
+
+/*
+ sckv;d
+ cvlvdv
+ vddvdvv
+ dvdvdv
+ */
+
+/*
+ <=
+ >=
+ ==
+ >
+ <
+ !=
+ */
+
+
+/*
+ var age = 15
+if age>=18{
+    print ("You can drink ")
+}
+*/
+
+
+/*
+ var name="Jame"
+if name == "James"{
+    print ("We have the same name")
+}
+else{
+    print("We don't have the same name :(")
+}
+ */
+
+
+/*
+var birth_year=1998
+if birth_year>=2000{
+    print("You are youngerthan 18 years")
+}
+else{
+    print("Welcome")
+}
+*/
+
+
+/*
+5+8
+2-7
+35*400
+100/2
+var a=6
+var b=3
+a+b
+a-b
+a*b
+a/b
+var c=8.0
+
+//Doubles are used to change a whole number into decimal
+// Cannot multiply Int with Double
+//If we say Double(a) it will change whole number to decimal
+//If we say Int(c) it will change decimal number to a whole
+//for decimal answers put "." something. For example:
+5.0/8.0
+*/
+/*
+var age:Int = 27
+var candy : String = "100 Grand"
+var weight :Double= 198.8
+*/
+
+
+//var sunny : Bool = true/false
+
+
+var sunny:Bool = true
+
+if sunny {
+    print("It's Sunny")
+} else {
+    print("Not Sunny")
+}
+
+
+
+
